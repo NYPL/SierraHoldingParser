6779505e1c24bb378d05c35c2602188a4f3e0acc
Mike Benowitz <mwbenowitz@gmail.com>
2020-07-31 12:56:03 -0400

    SCC-2189 Add deployment details and update env vars

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ def init
 
     $logger = NYPLRubyUtil::NyplLogFormatter.new(STDOUT, level: ENV['LOG_LEVEL'])
     $kms_client = NYPLRubyUtil::KmsClient.new
-    $avro_client = NYPLRubyUtil::NYPLAvro.by_name(ENV['RECORD_TYPE'].capitalize[..-2])
+    $avro_client = NYPLRubyUtil::NYPLAvro.by_name(ENV['SCHEMA_TYPE'])
     $kinesis_client = NYPLRubyUtil::KinesisClient.new({ :stream_name => ENV['KINESIS_STREAM'], :partition_key => 'id' })
     $location_client = LocationClient.new
 
